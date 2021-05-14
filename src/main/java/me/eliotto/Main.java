f50d8d4bf37d908a02ba3f3b5ed54901461a945c
Elias <eliasrpg369@gmail.com>
2021-05-14 23:56:47 +0200

    Initial commit

---
+++ src/main/java/me/eliotto/Main.java
@@ -0,0 +1,34 @@
+package me.eliotto;
+
+import me.eliotto.eventos.policia.Arrestar;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    String PREFIJO_NOMBRE = ChatColor.GRAY+"["+ChatColor.BLUE+getName()+ChatColor.GRAY+"]";
+
+    public void onEnable() {
+
+        Bukkit.getConsoleSender().sendMessage(PREFIJO_NOMBRE+ChatColor.GREEN+" El plugin ha sido iniciado correctamente");
+        registerEvents();
+    }
+
+    public void onDisable() {
+
+        Bukkit.getConsoleSender().sendMessage(PREFIJO_NOMBRE+ChatColor.GREEN+" El plugin ha sido cerrado correctamente");
+
+    }
+
+
+    public void registerEvents(){
+
+        PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(new Arrestar(), this);
+
+    }
+}
---
+++ src/main/java/me/eliotto/eventos/policia/Arrestar.java
@@ -0,0 +1,13 @@
+package me.eliotto.eventos.policia;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class Arrestar implements Listener {
+
+    @EventHandler
+    public void SiTePegaLucia(PlayerInteractEntityEvent event){
+
+    }
+}
